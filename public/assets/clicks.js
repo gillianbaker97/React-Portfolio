eb879be98359fce4075ce2fef47e0ed885d23364
Gillian Baker <gillianbaker97@gmail.com>
2021-09-07 14:02:22 -0400

    new

--- public/assets/clicks.js
+++ public/assets/clicks.js
@@ -1,9 +1,9 @@
-let card1 = document.getElementById("card1");
-card1.addEventListener("mouseover", function(event) {
-    event.target.style.opacity = .5;
+// let card1 = document.getElementById("card1");
+// card1.addEventListener("mouseover", function(event) {
+//     event.target.style.opacity = .5;
 
-    setTimeout(function() {
-        event.target.style.opacity = "";
-    }, 500);
-}, false);
+//     setTimeout(function() {
+//         event.target.style.opacity = "";
+//     }, 500);
+// }, false);
 
